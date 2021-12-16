b55b5d53cedbc73bf780b514e6011e2cede72589
bora001 <bora.vancouver@gmail.com>
2021-12-16 11:00:12 -0800

    slider

--- index.js
+++ index.js
@@ -8,3 +8,40 @@ window.addEventListener("scroll", function () {
     nav.classList.remove("sticker");
   }
 });
+
+const prevArrow = document.querySelector("#sec4 .arrow_left");
+const nextArrow = document.querySelector("#sec4 .arrow_right");
+const ReviewCnt = document.querySelector("#sec4 .content_box");
+const ReviewBox = document.querySelector("#sec4 .content_box .review_box");
+const Review = document.querySelectorAll(
+  "#sec4 .content_box .review_box .review"
+);
+
+const arrow = document.querySelectorAll("#sec4 .arrow");
+let count = 0;
+arrow.forEach((arr) => {
+  arr.addEventListener("click", function () {
+    if (arr.classList.contains("arrow_right")) {
+      count < Review.length - 1 ? count++ : "";
+    } else {
+      count > 0 ? count-- : "";
+    }
+
+    // btn
+    switch (count) {
+      case 0:
+        prevArrow.classList.add("off");
+        break;
+      case 3:
+        nextArrow.classList.add("off");
+        break;
+      default:
+        nextArrow.classList.remove("off");
+        prevArrow.classList.remove("off");
+        break;
+    }
+
+    const y = ReviewCnt.clientHeight;
+    ReviewBox.style.transform = `translateY(-${y * count}px)`;
+  });
+});
