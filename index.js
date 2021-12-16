2762b13baae01864eec5264b1a406fa1eb4d8c33
bora001 <bora.vancouver@gmail.com>
2021-12-16 13:37:55 -0800

    tab

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ window.addEventListener("scroll", function () {
     nav.classList.remove("sticker");
   }
 });
+// tab
+const tabBtn = document.querySelectorAll("#sec3 .tab_btns .btn");
+const tabBox = document.querySelectorAll("#sec3 .tab_box .tab");
+
+tabBtn.forEach((btn, i, arr) => {
+  tabBox[0].classList.add("active");
+  btn.addEventListener("click", function () {
+    let val = btn.attributes["data-tab"].value - 1;
+    tabBox.forEach((box) => box.classList.remove("active"));
+    tabBtn.forEach((btn) => btn.classList.remove("active"));
+
+    btn.classList.add("active");
+    tabBox[val].classList.add("active");
+    // tabBox[val].style.zIndex = "9";
+  });
+});
+
+//   box-shadow: 5px 5px 10px gray;
+
+// slide
 
 const prevArrow = document.querySelector("#sec4 .arrow_left");
 const nextArrow = document.querySelector("#sec4 .arrow_right");
@@ -16,7 +36,6 @@ const ReviewBox = document.querySelector("#sec4 .content_box .review_box");
 const Review = document.querySelectorAll(
   "#sec4 .content_box .review_box .review"
 );
-
 const arrow = document.querySelectorAll("#sec4 .arrow");
 let count = 0;
 arrow.forEach((arr) => {
