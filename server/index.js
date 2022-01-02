488c34ea9e8e7d75a2364e91162ceb7f10a2b3b4
bora001 <bora.vancouver@gmail.com>
2022-01-01 16:54:50 -0800

    server/mongoDB connected

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.listen(3000, () => {
-  console.log("this is server");
+  console.log("server is connected...");
 });
 
 app.use((req, res) => {
   res.send("this is server Page");
 });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose
+  .connect(process.env.mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("mongoDB is connected..."))
+  .catch((err) => console.log(err));
